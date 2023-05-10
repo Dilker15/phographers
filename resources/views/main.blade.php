42d8beaca4239c958aecd5ff9e47cf64fbde31fb
Dilker15 <dilker72@gmail.com>
2023-05-10 08:12:38 -0400

    bootstrap1

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -12,6 +12,8 @@
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
+    <link href="bootstrap/css/booystrap.min.css" rel="stylesheet" type="text/css">
+
 @stop
 
 @section('js')
